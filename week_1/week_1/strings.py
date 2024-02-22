024c31a7804e10d5327326698c139d3865784f01
elsi33 <elsienganga427@gmail.com>
2024-02-21 23:13:52 -0800

    lesson on strings, loops

---
+++ week_1/week_1/strings.py
@@ -0,0 +1,53 @@
+# strings in python
+# name : elsie wanjiku
+# date : 22/2/2024
+
+city = "nairobi"
+
+print(city[0]) 
+print(city[1]) 
+print(city[2]) 
+print(city[3]) 
+print(city[4])
+print(city[5])
+print(city[6]) 
+
+
+# conver to uppercase
+
+
+print(city)
+print("\n") # prints a new line
+print(city.upper())
+name = "ELSIE WANJIKU"
+print(name)
+print(name.lower()) # converts string to lower case
+
+town = "      Naivasha      "
+
+print(town)
+print("\t") # new tab
+print(town.strip())
+
+# add two strings
+
+f_name = "Elsie"
+s_name = "Wanjiku"
+
+full_name =  f_name + s_name
+
+print(full_name)
+
+# replacing a character
+
+fruit = "OrangOOOOO"
+
+print(fruit.replace("O" , "Y"))
+
+subject = "Physical:Sciences"
+print(subject.split(":"))
+
+age =30
+height = 1.6
+
+print("I am {0} years old and {1} meters tall " .format(age,height))
---
+++ week_2/loops.py
@@ -0,0 +1,19 @@
+# loops and iteration
+# date :22/2/2024
+# name :elsie wanjiku
+
+
+# for loop
+# while loop
+
+
+for number in range(0,10):
+    print(number)
+
+# squares of the number
+    
+for x in range(0,11):
+    print(x**2) # squares
+    print("\t")
+
+    print (str(x**2) + "\t" ,end=" ") # square
